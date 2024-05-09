7ef19965b7e3202febd8fab537cf8da6374ab1f6
Jack1Constantine@outlook.com <jack1constantine@outlook.com>
2024-05-09 09:03:32 +0800

    模拟键盘输入

---
+++ 模拟键盘输入.py
@@ -0,0 +1,9 @@
+import pyautogui
+# 获取当前屏幕分辨率
+
+# 设置鼠标位置为屏幕中心
+x = int(234)
+y = int(806)
+pyautogui.moveTo(x, y)
+pyautogui.leftClick()
+pyautogui.write("cloze1: FBLHDAMKEG cloze2: FCAEM KONHL inter: DCCABBAD 雅思:BCAADDD jewels prayers toughness furnishings change colour good luck charm")
